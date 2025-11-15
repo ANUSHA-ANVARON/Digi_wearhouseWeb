5644dd61a21db27b7e1f533ba591bfcaeb12917a
DomsGlobal <domsgloballlp@gmail.com>
2025-11-15 10:49:37 +0530

    added changes

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 // // server.js
-// import 'dotenv/config';
+// import "dotenv/config";
 // import express from "express";
 // import cors from "cors";
 // import handler from "./api/drape-saree-parts.js"; // import your function
 
-// console.log('Cloudinary Name:', process.env.CLOUDINARY_CLOUD_NAME);
-// console.log('Gemini Key:', process.env.GEMINI_API_KEY);
+// console.log("Cloudinary Name:", process.env.CLOUDINARY_CLOUD_NAME);
+// console.log("Gemini Key:", process.env.GEMINI_API_KEY);
 
 // const app = express();
 
