02dbb3269e01e866bec49044218039a03403fdba
sainy2001 <135034424+sainy2001@users.noreply.github.com>
2026-02-10 17:27:25 +0530

    %%%%%%%%%%%%%%%%

--- src/DigiWearHouseApp1/Pages/Registration.jsx
+++ src/DigiWearHouseApp1/Pages/Registration.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useApp } from "../context/Context";
 import digiLogoPoster from "../../assets/Digiware_logoPoster.png";
-import digi_logo from "../../assets/logo.png";
+import digi_logo from "../../assets/Logo.png";
 import { useNavigate } from "react-router-dom";
 import { ArrowLeft, ArrowRight } from "lucide-react";
 import {
@@ -1217,7 +1217,7 @@ const DigiWarehouseRegistration = () => {
                   Already have an account?
                   <button
                     onClick={navigateToLogin}
-                    className="text-[#800000] cursor-pointer hover:text-[#a00000] font-medium ml-1"
+                    className="text-[#6397A9] cursor-pointer hover:text-[#4F9EBB] font-medium ml-1"
                   >
                     Log in
                   </button>
